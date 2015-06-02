d07a49db542f88f18156dc08fab874d5ea761c28
Michael Stalker <mtstalker@gmail.com>
2015-06-02 15:28:02 -0400

    Add second part of Exercise 1.32

--- chapter_1/1.32.scm
+++ chapter_1/1.32.scm
@@ -1,7 +1,14 @@
-; Show that sum and product (Exercise 1.31) are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function:
+; 1. Show that sum and product (Exercise 1.31) are both special cases of a still
+; more general notion called accumulate that combines a collection of terms,
+; using some general accumulation function:
 ; (accumulate
 ;  combiner null-value term a next b)
-; Accumulate takes as arguments the same term and range specifications as sum and product, together with a combiner procedure (of two arguments) that specifies how the current term is to be combined with the accumulation of the preceding terms and a null-value that specifies what base value to use when the terms run out. Write accumulate and show how sum and product can both be defined as simple calls to accumulate.
+; Accumulate takes as arguments the same term and range specifications as sum
+; and product, together with a combiner procedure (of two arguments) that
+; specifies how the current term is to be combined with the accumulation of the
+; preceding terms and a null-value that specifies what base value to use when
+; the terms run out. Write accumulate and show how sum and product can both be
+; defined as simple calls to accumulate.
 
 ; Functions for testing term and next arguments
 (define (identity n) n)
@@ -33,3 +40,15 @@
 
 (define (product term a next b)
   (accumulate * 1 term a next b))
+
+
+; 2. If your accumulate procedure generates a recursive process, write one that
+; generates an iterative process. If it generates an iterative process, write
+; one that generates a recursive process.
+(define (accumulate-iteratively combiner null-value term a next b)
+  (define (iterate number result)
+    (if (> number b)
+      result
+      (iterate (next number)
+               (combiner (term number) result))))
+  (iterate a null-value))
