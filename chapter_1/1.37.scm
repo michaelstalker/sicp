21281674f9143d86235779ff2431b770e42c5044
Michael Stalker <mtstalker@gmail.com>
2015-06-16 10:41:18 -0400

    Add second part of exercise 1.37

--- chapter_1/1.37.scm
+++ chapter_1/1.37.scm
@@ -4,9 +4,9 @@
 ; N1 / (D1 + (N2 / (⋱ + (Nk / Dk)))).
 ; Suppose that n and d are procedures of one argument (the term index i) that return the Ni and Di of the terms of the continued fraction. Define a procedure cont-frac such that evaluating (cont-frac n d k) computes the value of the k-term finite continued fraction. Check your procedure by approximating 1/φ using
 
-(cont-frac (lambda (i) 1.0)
-	   (lambda (i) 1.0)
-	   k)
+; (cont-frac (lambda (i) 1.0)
+; 	   (lambda (i) 1.0)
+;	   k)
 
 ; for successive values of k. How large must you make k in order to get an approximation that is accurate to 4 decimal places?
 
@@ -20,9 +20,17 @@
 ; I had to make k equal 12 to get an approximation that is accurate to 4 decimal places.
 
 (define k 12)
-(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) k)
+(define (test i) 1.0)
+(cont-frac test test k)
 ; 0.6180257510729613
 
 ; 2. If your cont-frac procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
 
-
+(define (iterative-cont-frac n d k)
+  (define (iterator index accumulator)
+    (if (= index 0)
+	accumulator
+	(iterator (- index 1) (/ (n k) (+ (d k) accumulator)))))
+  (iterator k 0))
+(iterative-cont-frac test test k)
+; 0.6180257510729613
